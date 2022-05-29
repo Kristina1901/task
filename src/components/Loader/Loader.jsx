52434bebbbe4cce37019db9fbe90d99a5fa2f2e7
Kristina Tuzova <ktuzova1901@gmail.com>
2022-05-29 06:28:53 +0300

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,7 @@
 import { Oval } from 'react-loader-spinner';
+import s from 'components/Loader/Loader.module.css';
 const Loader = () => (
-  <div>
+  <div className={s.container}>
     <Oval height="48" width="48" color="#00BDD3" />
   </div>
 );
